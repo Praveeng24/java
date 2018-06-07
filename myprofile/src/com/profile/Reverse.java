7f4dab0cc09b4a46671c187b90c85cbd5afb814a
Praveeng24 <gpraveen241197@gmail.com>
2018-06-07 10:53:11 +0530

    Program

---
+++ myprofile/src/com/profile/Reverse.java
@@ -0,0 +1,21 @@
+package com.profile;
+
+public class Reverse {
+		public static void main(String[] args) {
+			int a=567890436;
+			
+			int[] array=new int[10];
+			for(int i=0;i<array.length;i++)
+			{
+				int t=a%10;
+		
+			
+				System.out.print(t);
+		a=a/10;
+			}
+		}
+}
+			
+		
+
+
